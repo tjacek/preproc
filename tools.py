8de5d036c1d9d6326d8595a2778910c612cb845f
tjacek <tjacek@agh.edu.pl>
2020-05-07 17:53:13 +0200

    sum img

--- tools.py
+++ tools.py
@@ -9,4 +9,11 @@ def time(in_path,out_path):
     transform=[proj.scale,helper]                
     imgs.transform(in_path,out_path,transform,single_frame=False)
 
-time("../MSR/ens1/box","../MSR/ens3/time")
+def sum_img(in_path,out_path):
+    def sum_fun(frames):
+        action_img=np.mean(frames,axis=0)
+        return action_img
+    imgs.action_img(in_path,out_path,sum_fun)
+
+#sum_img("proj/Z","action/Z")
+imgs.concat_seq("action/Y","action/Z","action/YZ")
