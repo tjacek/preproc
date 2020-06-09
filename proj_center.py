3dc239666363c3619374e2d9fac7bca2451d5050
tjacek <tjacek@agh.edu.pl>
2020-06-09 17:01:51 +0200

    CenterProj

--- proj_center.py
+++ proj_center.py
@@ -1,16 +1,27 @@
 import numpy as np,cv2
-import proj,imgs,pclouds
+import proj,imgs,pclouds,tools
 
-def proj_transform(in_path,out_path,dims=0):
-    def helper(frames):
+class CenterProj(object):
+    def __init__(self,dim,kern_size=(3,3)):
+        self.dim=dim
+        self.kern_size=kern_size
+
+    def __call__(self,frames):
         frames=[ proj.nonzero_points(frame_i) 
                     for frame_i in frames]
-  
-        new_frames=[proj.get_proj(pcloud_i.T,dims) for pcloud_i in frames]
-        new_frames=[proj.smooth_proj(frame_i) for frame_i in new_frames]
+        frames=center(frames)
+        new_frames=[proj.get_proj(pcloud_i.T,self.dim) 
+                        for pcloud_i in frames]
+        new_frames=[proj.smooth_proj(frame_i,self.kern_size) 
+                        for frame_i in new_frames]
         return new_frames
-    transform=[helper,proj.scale]    
-    imgs.transform(in_path,out_path,transform)
+
+def full_proj(seqs,out_path,kern_size=(3,3)):
+    def proj_factory(i):
+        funcs=[tools.median_smooth,CenterProj(i,kern_size),proj.scale]
+        return imgs.Pipeline(funcs)
+    proj_funcs=[proj_factory(i) for i in range(3)]
+    proj.proj_template(seqs,out_path,proj_funcs)
 
 def center(frames):
     center=pclouds.center_of_mass(frames)
@@ -21,9 +32,10 @@ def center(frames):
                     for pcloud_i in frames]
     return frames
 
-def gauss_smooth(img_i):
-    if(type(img_i)==list):
-        return [gauss_smooth(img_j)  for img_j in img_i]
-    return cv2.GaussianBlur(img_i, (9, 9), 0)
+#def gauss_smooth(img_i):
+#    if(type(img_i)==list):
+#        return [gauss_smooth(img_j)  for img_j in img_i]
+#    return cv2.GaussianBlur(img_i, (9, 9), 0)
 
-proj_transform("../MSR/box","testZ",2)
+if __name__=='__main__':
+    full_proj("../agum/box","../agum/full_center")
