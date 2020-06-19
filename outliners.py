6017ed3bd4d6759b0b5a9a53d0e7a61a6f607931
tjacek <tjacek@agh.edu.pl>
2020-06-19 12:59:40 +0200

    square y cord

--- outliners.py
+++ outliners.py
@@ -9,22 +9,34 @@ def outliner_transform(in_path,out_path):
     imgs.save_seqs(seqs,out_path)
 
 def outliner(frames):
+    print(len(frames))
     frames=[ proj.nonzero_points(frame_i) 
                     for frame_i in frames]
     center=pclouds.center_of_mass(frames)
     frames=[(pcloud_i.T-center.T).T for pcloud_i in frames]
     seq_min=proj.get_min(frames)
     seq_max=proj.get_max(frames)
+    max_y,min_y=seq_max[1],seq_min[1]
     def helper(points):
-#        neg=points[:,np.where(points[1,:]<0)]
-#        neg=np.squeeze(neg)
-#        pos=points[:,np.where(points[1,:]>0)]
-#        pos=np.squeeze(neg)
-
-#        points=np.concatenate([pos,neg],axis=1)
-#        raise Exception(points.shape)
+        neg=points[:,np.where(points[1,:]<0)]
+        pos=points[:,np.where(points[1,:]>=0)]
+        pos=square_scale(pos,max_y)
+        neg=np.squeeze(neg)
+        points=np.concatenate([pos,neg],axis=1)
+#        raise Exception(np.amax(points,axis=1))
+#        points=np.squeeze(points)
         points=np.array(proj.normalize(points))
         return pclouds.to_img(points)
     return [ helper(points) for points in frames]
+
+def square_scale(points,const):
+    y=points[1,:]
+    y/=const
+    y=y**2
+    y*=const
+#    y/= (const**2)
+    arrays=[points[0,:],y,points[2,:]]
+    new_points=np.concatenate(arrays,axis=0)
+    return new_points 
 
 outliner_transform("../agum/box","test")
