168fd3259a04724ccfc32e630419a39cb0f43925
tjacek <tjacek@student.agh.edu.pl>
2020-08-11 17:04:41 +0200

    many action imgs

--- action.py
+++ action.py
@@ -1,22 +1,43 @@
 import numpy as np
 import imgs,proj,outliners,tools
 
-def z_action_img(in_path,out_path):
+def many_action_img(in_path,out_path):
     def helper(frames):
-        frames=np.array([z_spot(i,img_i) for i,img_i in enumerate(frames)
-                               if(not img_i is None)])
-        action_i=np.mean(frames,axis=0)
-        print(action_i.shape)
-        action_i*=100.0
-#        action_i=action_i.astype(float)
-        return action_i
+        img0=simple_proj(frames)
+        img1=static_max(frames)
+        return np.concatenate([img0,img1],axis=0)
     funcs=[helper,proj.Scale()]
     imgs.action_img(in_path,out_path,funcs)
 
+def single_action_img(in_path,out_path):
+    funcs=[static_max,proj.Scale()]
+    imgs.action_img(in_path,out_path,funcs)
+
+def simple_proj(frames):
+    frames=np.array(frames)
+    frames[frames!=0]=1.0
+    return np.sum(frames,axis=0)
+
+def static_max(frames):
+    frames=np.array([z_spot(1.0,img_i) for i,img_i in enumerate(frames)
+                               if(not img_i is None)])
+    action_i=np.sum(frames,axis=0)
+    print(action_i.shape)
+    action_i[action_i!=0]=100.0
+    return action_i
+
+def time_max(frames):
+    frames=np.array([z_spot(i,img_i) for i,img_i in enumerate(frames)
+                               if(not img_i is None)])
+    action_i=np.mean(frames,axis=0)
+    print(action_i.shape)
+    action_i*=100.0
+    return action_i
+
 def z_spot(i,img_i):
     x,y=np.unravel_index(np.argmax(img_i),img_i.shape)
-    img_i=img_i.astype(float)
-    img_i.fill(0)
+#    img_i=img_i.astype(float)
+    img_i=np.zeros( img_i.shape,dtype=float)
     img_i[ x-5:x+5, y-5:y+5]=float(i)
 
     return img_i
@@ -43,4 +64,4 @@ def z_spot(i,img_i):
 #    transform=preproc+[helper,proj.Scale()]
 #    return imgs.Pipeline(transform)
 
-z_action_img("../MSR_exp1/box","../MSR_exp1/frames")
+many_action_img("../MSR_exp1/box","../MSR_exp1/frames")
