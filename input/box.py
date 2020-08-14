9ee8794307d434f4ae075ab2093004961ddc2dc2
tjacek <tjacek@agh.edu.pl>
2020-08-14 22:26:36 +0200

    equal box

--- input/box.py
+++ input/box.py
@@ -2,7 +2,7 @@ import numpy as np
 import imgs
 
 def box_frame(in_path,out_path):
-    fun=[extract_box]#,norm_z]
+    fun=[equal_box]#[extract_box]
     imgs.transform(in_path,out_path,fun,single_frame=False)
 
 def norm_z(frames,max_value=240.0):
@@ -16,6 +16,24 @@ def norm_z(frames,max_value=240.0):
     frames=np.abs(frames-max_value)
     frames[zero_points]=0.0
     return frames
+
+def equal_box(frames):
+    frames=extract_box(frames)
+    def helper(img_i):
+        x,y=img_i.shape
+        ratio=x/y
+        if(ratio<1):
+            img_i=img_i.T
+            x,y=y,x
+        delta=x-y
+        parity=delta % 2
+        x_img=np.zeros((x,int(delta/2)))
+        y_img=np.zeros((x,int(delta/2+parity)))
+        img_i=np.concatenate([x_img,img_i,y_img],axis=1)
+        if(ratio<1):
+            img_i=img_i.T
+        return img_i
+    return [helper(frame_i) for frame_i in frames]
 
 def extract_box(frames):
     bounds=[ frame_bounds(frame_i) for frame_i in frames]
