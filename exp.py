e89f727660b40cfcdc20791dfb09fe11dac9eeeb
tjacek <tjacek@agh.edu.pl>
2020-09-08 16:18:36 +0200

    action img

--- exp.py
+++ exp.py
@@ -1,21 +1,17 @@
+import numpy as np
 import os.path
-#import proj_center,files,imgs
 #import input.binary
+import imgs,proj
 import input.box,tools,agum.seqs
 
-#def gen_proj(in_path):
-#    dir_path=os.path.split(in_path)[0]
-#    proj_path=dir_path+"/proj"
-#    files.make_dir(proj_path)
-#    names=["xy","zy","xz"]
-#    names=[ "%s/%s" % (proj_path,name_i) for name_i in names]
-#    for i,name_i in enumerate(names):
-#        proj_center.proj_transform(in_path,name_i,i)
-#    tmp_path="%s/%s" % (proj_path,"tmp")   
-#    imgs.concat_seq(names[1],names[2],tmp_path)
-#    full_path="%s/%s" % (proj_path,"full")   
-#    imgs.concat_seq(names[0],tmp_path,full_path)
 #input.binary.h5_convert("../UWA/raw","../UWA/depth")
+
+def action_exp(in_path,out_path):
+    def helper(frames):
+        action_img=np.mean(frames,axis=0)
+        return action_img#.astype('uint8')
+    fun=[helper,proj.Scale()]
+    imgs.action_img(in_path,out_path,fun)
 
 def exp(in_path):
     dir_path=os.path.split(in_path)[0]
@@ -26,4 +22,4 @@ def exp(in_path):
     agum_fun=agum.seqs.get_seq_agum()
     agum_fun(paths["scale"],paths["agum"])
 
-exp("../depth")
+action_exp("../box","../exp/action")
