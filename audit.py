48288fae3c752fb830ca2f286b106ded7daddf7d
tjacek <tjacek@agh.edu.pl>
2020-10-04 01:45:22 +0200

    check size of action imgs

--- audit.py
+++ audit.py
@@ -1,6 +1,14 @@
 import imgs
 
-in_path="../box"
-img_dict=imgs.read_seqs(in_path)
-for seq_i in img_dict.values():
-    print(seq_i[0].shape)	
+def seq_len(in_path):
+	img_dict=imgs.read_seqs(in_path)
+	for seq_i in img_dict.values():
+		print(seq_i[0].shape)
+
+def action_shape(in_path):
+	frames=imgs.read_frames(in_path)
+	for frame_i in frames:
+		print(frame_i.shape)	
+
+in_path="../msr_rank"
+action_shape(in_path)
