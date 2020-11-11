b0ca42b634fe769da40db662a9606aa24256c7ac
tjacek <tjacek@agh.edu.pl>
2020-11-11 20:02:58 +0100

    reformat skeleton

--- script.py
+++ script.py
@@ -31,9 +31,17 @@ def conv_seq(in_path,out_path):
 				for line_i in lines]
 		seq_i=np.array([np.fromstring(line_i,sep=",",dtype=float) 
 					for line_i in lines])
+		seq_i= skeleton_reformat(seq_i)
 		out_i=files.replace_path(path_i,out_path)
 		out_i=out_i.split(".")[0]
 		print(out_i)
 		np.save(out_i,seq_i)
+
+def skeleton_reformat(seq_i,n_joints=20):
+	ts_len=int(seq_i.shape[0]/n_joints)
+	seqs=[seq_i[(i*ts_len):((i+1)*ts_len),:] 
+	    	for i in range(n_joints)]
+	seqs=np.concatenate(seqs,axis=1)
+	return seqs
 
 conv_seq("../MSR_skeleton","skeleton")
